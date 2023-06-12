3a481d826cc96635d3d6d09ff339a738851e40ce
Damian Peiris <drpeiris3@gmail.com>
2023-06-12 22:55:33 +0530

    Image search - done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-$(document).ready(() => {
+$(document).ready(()=>{
     loadMainImage();
+    setTimeout(fetchPhotos,3000);
 
 });
 
@@ -7,7 +8,7 @@ async function loadMainImage() {
 
 
     try {
-        let response = fetch("https://api.unsplash.com/photos/?client_id=lz0WtbT_YAZdZKUvfjBLkO9Fifnhw6y9S4kYJx7cj0A&per_page=10&order_by=latest&w=100vw&height=70vh");
+        let response = fetch("https://api.unsplash.com/photos/?client_id=lz0WtbT_YAZdZKUvfjBLkO9Fifnhw6y9S4kYJx7cj0A&per_page=10&order_by=latest&w=100vw&height=80vh");
         let imageData = await response;
         let jsonData = imageData.json();
         let finalData = await jsonData;
@@ -15,12 +16,62 @@ async function loadMainImage() {
         $(".searchContainer").css("background-image", "url(" + finalData[element].urls.full + ")")
 
 
-
     } catch (e) {
         swal("", "Something happened!" + e, "error")
     }
 }
 
+
+async function fetchPhotos() {
+    try {
+        let response = fetch("https://api.unsplash.com/photos/?client_id=lz0WtbT_YAZdZKUvfjBLkO9Fifnhw6y9S4kYJx7cj0A&page=30&order_by=latest");
+        let imageData = await response;
+        let json = imageData.json();
+        let finalData = await json;
+        finalData.map((photo) => {
+            $(".photoContainer").append("<img  class='child' src=" + photo.urls.regular + " data-aos = zoom-in>")
+
+        });
+
+
+    } catch (e) {
+        swal("", "Something happened!" + e, "error")
+    }
+
+}
+
 if (window.innerWidth < 800) {
     $("#searchBar").attr("placeholder", " 🔍 Search for images!");
+}
+$("#searchBar").on("keydown", (event) => {
+    if (event.key === "Enter") {
+        searchPhotos($("#searchBar").val())
+    }
+
+})
+
+async function searchPhotos(query) {
+    try {
+        let response = fetch("https://api.unsplash.com/search/photos/?client_id=lz0WtbT_YAZdZKUvfjBLkO9Fifnhw6y9S4kYJx7cj0A&query="+query);
+        let imageData = await response;
+        let json = imageData.json();
+        let finalData = await json;
+        $(".photoContainer").empty();
+        /*Assigning the results array to a variable.*/
+        let searchedImage = finalData.results;
+        /*Looping the array and appending the search results.*/
+        searchedImage.map((image)=>{
+            $(".photoContainer").append("<img  class='child' src=" + image.urls.regular + " data-aos = zoom-in" + ">");
+        });
+
+
+
+
+        swal("", "Showing results for " + query, "success")
+        $("#searchBar").val("");
+    } catch (e) {
+        swal("", "Something happened!" + e, "error")
+    }
+
+
 }
